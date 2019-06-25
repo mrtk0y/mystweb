684cf3db20bcff890f259ce52c91924631ef2575
Thinh <cloudskyfog@gmail.com>
2019-06-25 14:56:36 +0700

    Hoan thien bai weatherapp command line + chuyen sang bai dynamic site

--- homework/js/weather.js
+++ homework/js/weather.js
@@ -1,2 +1,44 @@
+const https = require('https');
+const http = require('http');
+function printMessage(city, temp) {
+    const message = `The temp of ${city} is ${temp} F`;
+    console.log(message);
+}
 
-module.export
+function printError (error) {
+    console.error(error.message);
+}
+
+function getTemp (city) {
+    try{
+        const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=27e8a192a08c2cb50fc6139144dcd6fe`, response => {
+            if (response.statusCode === 200){
+            let body = "";
+            let weather = "";
+            //Read data
+            response.on('data', data1 => {
+                body += data1.toString();
+            });
+
+            //Parse data
+            response.on('end', () => {
+                try {
+                    weather = JSON.parse(body);
+                    printMessage(city, weather.main.temp);
+                } catch (error) {
+                        printError(error);
+                }
+            });
+            } else {
+                const message =`There was an error getting the Temp for ${city} (${http.STATUS_CODES[response.statusCode]}) `;
+                const statusCodeError = new Error (message);
+                printError(statusCodeError);
+            };
+        });
+        request.on('error', printError) 
+    } catch(error) {
+            printError(error);
+    }
+}
+
+module.exports.getTemp = getTemp;
--- homework/js/weatherapp.js
+++ homework/js/weatherapp.js
@@ -1,29 +1,7 @@
 
-// Function to get the HTTPS module
-const https = require('https');
+// Function to get the Temperature of City module
+const weather = require('./weather.js')
 
-function printMessage(city, temp){
-    const message = `The temp of ${city} is ${temp} F`;
-    console.log(message);
-}
-
-function getTemp (city) {
-    const request = https.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=27e8a192a08c2cb50fc6139144dcd6fe`
-    , response => {
-        let body = "";
-        let weather = "";
-        //Read data
-        response.on('data', data1 => {
-            body += data1.toString();
-        });
-
-        //Parse data
-        response.on('end', () => {
-            weather = JSON.parse(body);
-            printMessage(city, weather.main.temp);
-            ;
-        });
-    });
-};
 const city = process.argv.slice(2);
-city.forEach(getTemp);
+city.forEach(weather.getTemp);
+
