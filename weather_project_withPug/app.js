ea85d94d7f51a54559d181bf59185e646113d50d
Thinh <cloudskyfog@gmail.com>
2019-07-23 19:42:31 +0700

    Route Web

--- weather_project_withPug/app.js
+++ weather_project_withPug/app.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const cookieParser =  require('cookie-parser');
 const path=require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname,"views")));
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
+// app.use(cookieParser());
 app.set('view engine', 'pug');
 
 app.get('/', (request, response) => {
   response.render('index');
 });
 
-// app.get('/weather', (request, response) => {
-//   response.render('weather')
-// })
+app.post('/weather', (request, response) => {
+  response.render('weather');
+})
+
+app.get('/user', (request, response) => {
+  response.render('user', { name: request.cookies.username});
+})
 
 app.post('/user', (request, response) => {
-  response.render('user');
+  response.cookie('username', request.body.username);
+  response.render('user',{ name: request.body.username });
+  
 })
 
 app.listen(3000, ()=> {
