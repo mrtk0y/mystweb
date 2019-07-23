2ad347a0091c0d0ab00942fbd5799156be701252
Thinh <cloudskyfog@gmail.com>
2019-07-23 18:53:09 +0700

    Them user weather pug

---
+++ express_basics/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path=require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,"views")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'pug');
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.post('/weather', (request, response) => {
+  response.render('weather')
+});
+
+app.post('/user', (request, response) => {
+  response.render('user');
+});
+
+app.listen(3000, ()=> {
+  console.log('Sever is running now at port 3000');
+});
--- weather_project_withPug/app.js
+++ weather_project_withPug/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path=require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname,"views")));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.set('view engine', 'pug');
 
@@ -10,11 +13,14 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
-app.get('/example', (request, response) => {
-  response.send("Helllo");
-  response.end();
-});
+// app.get('/weather', (request, response) => {
+//   response.render('weather')
+// })
+
+app.post('/user', (request, response) => {
+  response.render('user');
+})
 
 app.listen(3000, ()=> {
   console.log('Sever is running now at port 3000');
-})
+});
