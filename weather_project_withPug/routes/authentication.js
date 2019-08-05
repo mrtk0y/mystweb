6e5dbe7a541e23babde02770bf3e00091e090613
Thinh <cloudskyfog@gmail.com>
2019-08-06 02:09:57 +0700

    Modified 6/8

--- weather_project_withPug/routes/authentication.js
+++ weather_project_withPug/routes/authentication.js
@@ -5,5 +5,11 @@ router.get('/', (request, response) => {
   response.render('login');
 });
 
+router.post('/login', (request, response, next)=> {
+  return response.send('logged in');
+});
 
+router.post('/user', (request, response, next)=> {
+  response.render('index');
+})
 module.exports = router;
